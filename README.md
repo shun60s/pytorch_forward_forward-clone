a31a061aeea594e7aa4b7881e43372bfe7241d97
shun <36104188+shun60s@users.noreply.github.com>
2024-05-07 14:04:05 +0900

    revise

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # pytorch_forward_forward clone   
 
-This is cloned from  [pytorch_forward_forward](https://github.com/mpezeshki/pytorch_forward_forward) and some changes.    
+This is cloned from  [pytorch_forward_forward](https://github.com/mpezeshki/pytorch_forward_forward) and some changes. This is non-CUDA (CPU-only) version.       
 
 
 ## usage  
 
 See the original pytorch_forward_forward README-pytorch_forward_forward.md.   
 [Forward-Forward Algorithm by Geoffrey Hinton.pdf](https://www.cs.toronto.edu/~hinton/FFA13.pdf) is a paper about forward_forward idea.  
+  
 
+![org, pos, neg - samples](./imgs/Figure.png)  
+  
+![result, error rate](./imgs/gamen.png)
 
 ## License  
 
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ This is a copy from  [pytorch_forward_forward](https://github.com/mpezeshki/pyto
 Please see LICENSE_pytorch_forward_forward. 
 
 """
+"""
+This is CPU  beside original uses CUDA.
+Check version:
+    Python 3.6.4 on win32
+    torch  1.7.1+cpu
+    torchvision 0.8.2+cpu
+    tqdm  4.28.1
+    matplotlib 3.3.1
+""" 
 
 import matplotlib.pyplot as plt
 import torch
@@ -14,6 +23,9 @@ from torch.optim import Adam
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
 from torch.utils.data import DataLoader
+
+import argparse # add
+import sys      # add
 
 
 def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
@@ -42,18 +54,19 @@ def overlay_y_on_x(x, y):
     """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
     """
     x_ = x.clone()
-    x_[:, :10] *= 0.0
-    x_[range(x.shape[0]), y] = x.max()
+    # change from the first 10 pixels to bottom line 10 pixels
+    x_[:, 28*27 : 28*27+10] *= 0.0
+    x_[range(x.shape[0]), 28*27 + y] = x.max()  # # x.max() is 2.8215
     return x_
 
 
 class Net(torch.nn.Module):
 
-    def __init__(self, dims):
+    def __init__(self, dims, epochs, threshold ):
         super().__init__()
         self.layers = []
         for d in range(len(dims) - 1):
-            self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
+            self.layers += [Layer(dims[d], dims[d + 1], epochs, threshold)]  #.cuda()]  # change
 
     def predict(self, x):
         goodness_per_label = []
@@ -75,16 +88,16 @@ class Net(torch.nn.Module):
 
 
 class Layer(nn.Linear):
-    def __init__(self, in_features, out_features,
-                 bias=True, device=None, dtype=None):
-        super().__init__(in_features, out_features, bias, device, dtype)
+    def __init__(self, in_features, out_features,epochs, threshold, # change
+                 bias=True):  # , device=None, dtype=None):  # change
+        super().__init__(in_features, out_features, bias)  # , device, dtype) # change
         self.relu = torch.nn.ReLU()
         self.opt = Adam(self.parameters(), lr=0.03)
-        self.threshold = 2.0
-        self.num_epochs = 1000
+        self.threshold = threshold  # 2.0   # change
+        self.num_epochs = epochs    # 1000  # change
 
     def forward(self, x):
-        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)  # # normalize
         return self.relu(
             torch.mm(x_direction, self.weight.T) +
             self.bias.unsqueeze(0))
@@ -112,27 +125,72 @@ def visualize_sample(data, name='', idx=0):
     plt.title(name)
     plt.imshow(reshaped, cmap="gray")
     plt.show()
+  
+""" add start """
+def visualize_sample2(data, name, idx=0):
+    fig=plt.figure()
+    for i in range(len(data)):  # len(data) must be 3.
+        plt.subplot(int(131+i))
+        plt.title(name[i])
+        plt.imshow(data[i][idx].cpu().reshape(28, 28), cmap="gray")
+        plt.minorticks_on()
+        #plt.grid()
+    fig.tight_layout()
+    plt.show()
+""" add end """
     
     
 if __name__ == "__main__":
-    torch.manual_seed(1234)
-    train_loader, test_loader = MNIST_loaders()
-
-    net = Net([784, 500, 500])
-    x, y = next(iter(train_loader))
-    x, y = x.cuda(), y.cuda()
-    x_pos = overlay_y_on_x(x, y)
-    rnd = torch.randperm(x.size(0))
-    x_neg = overlay_y_on_x(x, y[rnd])
+    """ add start """
+    parser = argparse.ArgumentParser(description='pytorch forward forward ')
+    parser.add_argument('--train-batch-size', type=int, default=50000, metavar='N',
+                        help='input batch size for training (default: 50000)')
+    parser.add_argument('--test-batch-size', type=int, default=10000, metavar='N',
+                        help='input batch size for testing (default: 10000)')
+    parser.add_argument('--epochs', type=int, default=1000, metavar='N',
+                        help='number of epochs to train (default: 1000)')
+    parser.add_argument('--threshold', type=float, default=2.0, metavar='TH',
+                        help='threshold  (default: 2.0)')
+    parser.add_argument('--no-cuda', action='store_true', default=True, help='disables CUDA training')
+    parser.add_argument('--save-model', action='store_true', default=True,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    """ end of add """
     
-    for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
-        visualize_sample(data, name)
+    torch.manual_seed(1234)
+    train_loader, test_loader = MNIST_loaders(args.train_batch_size, args.test_batch_size) # change
+
+    # # input dimension 784 is 28x28, MNIST data size.
+    net = Net([784, 500, 500], args.epochs, args.threshold ).to(device) # change
+    x, y = next(iter(train_loader))
+    
+    if use_cuda:      # add
+        x, y = x.cuda(), y.cuda()
+        
+    x_pos = overlay_y_on_x(x, y)
+    rnd = torch.randperm(x.size(0))  # # x.size(0) is train_batch_size
+    # change where y==y[rnd], increase one as label number. 10 becomes 0.
+    x_neg = overlay_y_on_x(x, torch.where( torch.where(y == y[rnd], y+1, y[rnd]) == 10, 0, torch.where(y == y[rnd], y+1, y[rnd])) )
+    
+    """ change start """
+    #for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
+    #    visualize_sample(data, name)
+    # show two samples.
+    visualize_sample2([x, x_pos, x_neg], ['orig', 'pos', 'neg'], idx=0) 
+    visualize_sample2([x, x_pos, x_neg], ['orig', 'pos', 'neg'], idx=4) 
+    """ change end """
     
     net.train(x_pos, x_neg)
-
-    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
-
+    
+    print('train error:[%]', (1.0 - net.predict(x).eq(y).float().mean().item()) * 100 )  # change
+    
     x_te, y_te = next(iter(test_loader))
-    x_te, y_te = x_te.cuda(), y_te.cuda()
-
-    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
+    if use_cuda:      # add
+        x_te, y_te = x_te.cuda(), y_te.cuda()
+    
+    print('test error:[%]', (1.0 - net.predict(x_te).eq(y_te).float().mean().item()) * 100)  # change
+    
+    if args.save_model:   # add
+        torch.save(net.state_dict(), "mnist_forward_forward.pt")  # add
